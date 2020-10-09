e3f142008022c4bf0fc72a8764fd713ab06cece0
RSakultarawattn <ray.sakultarawattn@gmail.com>
2020-10-08 17:36:46 -0700

    start for today

---
+++ admin/admin.js
@@ -0,0 +1,35 @@
+const form = document.querySelector('form');
+import { PRODUCTS } from '../constants.js';
+import {getLocalStorageSouls } from '../utils.js';
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const data = new FormData(form);
+
+    const id = data.get('id');
+    const name = data.get('name');
+    const image = data.get('image');
+    const quantity = data.get('quantity');
+    const price = data.get('price');
+
+
+    const newSoul = {
+
+        id: id,
+        name: name,
+        image: image,
+        quantity: quantity,
+        price: Number(price),
+
+
+
+    };
+
+    const localStorageSouls = getLocalStorageSouls();
+    localStorage.push(newSoul);
+
+    const stringyLocalSouls = JSON.stringify(localStorageSouls);
+    localStorage.setItem(PRODUCTS, stringyLocalSouls);
+
+})
